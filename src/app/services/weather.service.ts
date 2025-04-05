2325063b6300cfd55836a833604da119edbc0f0a
Lorenzo Rapposelli <rapposelli.lorenzo@gmail.com>
2025-04-05 19:54:27 +0200

    rimosse la visualizzazione delle ore appartenenti ai giorni successi

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -23,7 +23,7 @@ export class WeatherService {
   }
 
   searchMeteoByCoords(latitude: number, longitude: number): Observable<any> {
-    const apiResponse = this.http.get<any>(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_direction_10m,is_day&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_direction_10m&timezone=auto&past_hours=1&forecast_hours=24`)
+    const apiResponse = this.http.get<any>(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_direction_10m,is_day&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_direction_10m,is_day&timezone=auto&forecast_hours=24`)
 
     return apiResponse
   }
@@ -47,7 +47,7 @@ export class WeatherService {
   }
 
   fixedCurrentData(currentMeteo: any) {
-    return this.current = {
+    this.current = {
       temperature: Math.round(currentMeteo.temperature_2m),
       appTemperature: Math.round(currentMeteo.apparent_temperature),
       precipitation: currentMeteo.precipitation,
@@ -57,34 +57,38 @@ export class WeatherService {
       date: currentMeteo.time,
       icon: `/${this.meteoMap.get(currentMeteo.weather_code)}.png`
     }
+    if(currentMeteo.isday){
+      this.current.icon = `/${this.meteoNightMap.get(currentMeteo.weather_code)}.png`
+    }
+    return this.current
   }
 
   fixedHourlyData(hourlyMeteo: any) {
     this.hourly = []
+    const dayNumber = new Date()
+
     for(let i=0; i < hourlyMeteo.time.length; i++){
-      const hour = {
-        id: i+1,
-        temperature: Math.round(hourlyMeteo.temperature_2m[i]),
-        appTemperature: Math.round(hourlyMeteo.apparent_temperature[i]),
-        precipitation: hourlyMeteo.precipitation[i],
-        windSpeed: hourlyMeteo.wind_speed_10m[i],
-        windDirection: hourlyMeteo.wind_direction_10m[i],
-        date: hourlyMeteo.time[i],
-        isday: hourlyMeteo.is_day[i],
-        icon: ``
+      const hourDate = new Date(hourlyMeteo.time[i])
+      if(dayNumber.getDay() === hourDate.getDay()){
+        const hour = {
+          id: i+1,
+          temperature: Math.round(hourlyMeteo.temperature_2m[i]),
+          appTemperature: Math.round(hourlyMeteo.apparent_temperature[i]),
+          precipitation: hourlyMeteo.precipitation[i],
+          windSpeed: hourlyMeteo.wind_speed_10m[i],
+          windDirection: hourlyMeteo.wind_direction_10m[i],
+          date: hourlyMeteo.time[i],
+          isday: hourlyMeteo.is_day[i],
+          icon: ``
+        }
+        hourlyMeteo.is_day[i] ? 
+          hour.icon = `/${ this.meteoMap.get(hourlyMeteo.weather_code[i]) }.png` : 
+          hour.icon = `/${ this.meteoNightMap.get(hourlyMeteo.weather_code[i])}.png`
+  
+        this.hourly.push(hour)
       }
 
-        // if(!hourlyMeteo.is_day[i]){
-        //   hour.icon = `/${this.meteoNightMap.get(hourlyMeteo.weather_code[i])}.png`
-        // }else{
-        //   hour.icon = `/${this.meteoMap.get(hourlyMeteo.weather_code[i])}.png`
-        // }
-
-        hourlyMeteo.is_day[i]? hour.icon = `/${this.meteoMap.get(hourlyMeteo.weather_code[i])}.png` : hour.icon = `/${this.meteoNightMap.get(hourlyMeteo.weather_code[i])}.png`
-
-      this.hourly.push(hour)
     }
-    console.log(this.hourly)
     return this.hourly
   }
 
